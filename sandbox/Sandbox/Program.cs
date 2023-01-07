05865a26e6199c45897bc589036a4c8cdd44d64b
NtombiHontyo <u252121986@byui.edu>
2023-01-07 10:13:14 +0200

    roughwork and assignment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Let's kill this C#!!");
+        Console.Write("What is your desired mark for C#? ");
+        string color = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"You will get {color} if you work hard.");
     }
 }
