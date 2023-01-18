059c127e8714cc1ca312236c425f73718e62f0d5
NtombiHontyo <u252121986@byui.edu>
2023-01-19 00:28:22 +0200

    Week3 Preparation

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        
+
+        Job job2 = new Job();
+        job2._jobTitle = "Data Analyst";
+        job2._company = "Mercedes-Benz SA";
+        job2._startYear = 2023;
+        job2._endYear = 2029;
+        
+
+        Resume resume1 = new Resume();
+        resume1._name = "Ntombesizwe Hontyo";
+        resume1._jobList.Add(job1);
+        resume1._jobList.Add(job2);
+        resume1.DisplayResume();
+        
+        
     }
+
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,23 +4,5 @@ class Program
 {
     static void Main(string[] args)
     {
-
-        int digit = -1;
-        List<int> words = new List<int>();
-        do
-        {
-        Console.Write("enter a number: ");
-        string entry = Console.ReadLine();
-        digit = int.Parse(entry);
-        words.Add(digit);
-        }while (digit != 0);
-
-        
-        for (int i = 0; i < words.Count - 1; i++)
-        {
-            Console.WriteLine(words[i]);
-        }
-
-        
     }
-}
+}    
